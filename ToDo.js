bce7554f34a4df7f2bb1c9a51af7afc31240ea92
joaovictor <joaovictor0101@gmail.com>
2024-12-27 10:51:55 -0300

    Check feature adjustment

--- ToDo.js
+++ ToDo.js
@@ -5,11 +5,12 @@ let styleContainer = document.getElementsByClassName("StyleContainerTasks");
 let top1 = document.getElementById("Top1");
 let i = 0
 function creatAsk(){
+  i++
     let task = `<div class="feedback">
     <div class="checkStyle">
-      <input onclick="CheckEffect()" type="checkbox" id="feedback-checkbox"/>
+      <input onclick="CheckEffect(${i})" type="checkbox" id="feedback-checkbox"/>
       </div>
-      <div id="Check1" class="nameTaskStyle">
+      <div id="Check${i}" class="nameTaskStyle">
       <label for="feedback-checkbx">${bar.value}</label>
       </div>
       <div class="deleteStyle">
@@ -30,8 +31,9 @@ tempContainer.innerHTML = task;
 }
 button.addEventListener("click",creatAsk)
 
-function CheckEffect(){
-  let Check1= document.getElementById("Check1")
+function CheckEffect(i){
+  let Check1= document.getElementById("Check"+i);
+  
   console.log(Check1)
   
   Check1.classList.add("TaskDone");
